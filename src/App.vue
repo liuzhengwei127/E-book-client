74a74f9bcbb659fc8e23bdcf4d5089d5a3952ee2
liuzhengwei127 <756670011@qq.com>
2019-02-25 19:58:34 +0800

    add bootstrap.css and static Login page

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Login/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Login from './pages/Login/Login'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Login
   }
 }
 </script>
---
+++ src/pages/Login/Login.vue
@@ -0,0 +1,29 @@
+<template>
+    <body class="text-center container">
+    <form  class="form-signin">
+        <h1 class="h2 font-weight-normal mb-3">请先登录</h1>
+        <input type="username" class="form-control mb-3" placeholder="用户名" required>
+        <input type="password" class="form-control mb-1" placeholder="密码" required>
+        <div class="checkbox mb-2">
+            <label>
+                <input type="checkbox" value="remember-me"> 记住我
+            </label>
+        </div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">登录</button>
+        <p class="mt-5 mb-3 text-muted">&copy; 2019 上海交通大学软件学院 柳正威</p>
+    </form>
+    </body>
+</template>
+
+<script>
+    export default {
+        name: "Login"
+    }
+</script>
+
+<style scoped>
+    .form-signin
+    {
+        padding: 25%;
+    }
+</style>
