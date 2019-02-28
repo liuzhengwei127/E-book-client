fe1752df66d3a8c28337e51cc85328761f4ad892
liuzhengwei127 <756670011@qq.com>
2019-02-28 12:11:52 +0800

    href of contact

--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -4,14 +4,14 @@
             <div class="container">
                 <div class="row">
                     <div class="col-sm-8 col-md-7 py-4">
-                        <h4 class="text-white">About</h4>
+                        <h4 class="text-white">关于</h4>
                         <p class="text-muted">Add some information about the album below, the author, or any other background context. Make it a few sentences long so folks can pick up some informative tidbits. Then, link them off to some social networking sites or contact information.</p>
                     </div>
                     <div class="col-sm-4 offset-md-1 py-4">
-                        <h4 class="text-white">Contact</h4>
+                        <h4 class="text-white">联系</h4>
                         <ul class="list-unstyled">
-                            <li><a href="#" class="text-white">Follow on Github</a></li>
-                            <li><a href="#" class="text-white">Email me</a></li>
+                            <li><a href="https://github.com/liuzhengwei127" class="text-white">Follow on Github</a></li>
+                            <li><a href="https://mail.qq.com/" class="text-white">Email me</a></li>
                         </ul>
                     </div>
                 </div>
