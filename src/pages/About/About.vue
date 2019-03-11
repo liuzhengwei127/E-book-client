1ab5e99aa2e2878361fec5086db3ee22b8e2f451
liuzhengwei127 <756670011@qq.com>
2019-03-11 16:21:32 +0800

    about page

--- src/pages/About/About.vue
+++ src/pages/About/About.vue
@@ -1,6 +1,17 @@
 <template>
-    <div>
-        About us
+    <div class="container">
+        <h1>关于</h1>
+        <hr>
+        <div>
+            <div>
+                <span>
+                    Follow me on
+                    <i class="iconfont icon-github"></i>
+                    <a href="https://www.github.com/liuzhengwei127">Github</a>
+                </span>
+
+            </div>
+        </div>
     </div>
 </template>
 
