34270c3c8bcdd479ccd43f10b7fa59ed7fe7acfd
liuzhengwei127 <756670011@qq.com>
2019-03-28 13:19:51 +0800

    static page of Search page

--- src/pages/Search/Search.vue
+++ src/pages/Search/Search.vue
@@ -11,14 +11,29 @@
             <button class="btn btn-primary btn-block ml-2 col-md-1">搜索</button>
         </div>
         <div>
-
+            <ul  class="mt-5">
+                <li v-for="(book, index) in books" :key="index" class="list-unstyled mb-5">
+                    <Book :book="book" :index="index"></Book>
+                </li>
+            </ul>
         </div>
     </div>
 </template>
 
 <script>
+    import {mapState} from 'vuex'
+    import Book from '../../components/Book/Book'
+
     export default {
-        name: "Search"
+        name: "Search",
+        components: {
+            Book,
+        },
+        computed: {
+            ...mapState({
+                books: state => state.Books.books
+            })
+        },
     }
 </script>
 
