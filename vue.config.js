9da81f7c1746e9cdfc542ff57d10c3bace747845
liuzhengwei127 <756670011@qq.com>
2019-04-11 19:47:55 +0800

    proxy target changed

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
         // 跨域代理
         proxy: {
             '/api': {
-                target: 'http://localhost:1211/',
+                target: 'http://47.100.236.223:1211/',
                 changeOrigin: true,
                 ws: true,
                 pathRewrite: {
