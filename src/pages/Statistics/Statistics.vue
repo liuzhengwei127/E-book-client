e12518b932f1c2ef509bdd54f8cb0e3546b600f3
liuzhengwei127 <756670011@qq.com>
2019-04-13 16:01:30 +0800

    limit time span among one month (half)

--- src/pages/Statistics/Statistics.vue
+++ src/pages/Statistics/Statistics.vue
@@ -6,6 +6,8 @@
                 <el-date-picker
                         v-model="beginDate"
                         type="date"
+                        value-format="yyyy-MM-dd"
+                        :picker-options="beginPickerOptions"
                         placeholder="选择日期">
                 </el-date-picker>
             </el-col>
@@ -14,6 +16,8 @@
                 <el-date-picker
                         v-model="endDate"
                         type="date"
+                        value-format="yyyy-MM-dd"
+                        :picker-options="endPickerOptions"
                         placeholder="选择日期">
                 </el-date-picker>
             </el-col>
@@ -26,8 +30,56 @@
         name: "Statistics",
         data() {
             return {
-                beginDate: undefined,
-                endDate: undefined,
+                beginDate: null,
+                endDate: null,
+                beginPickerOptions: {
+                    disabledDate: (time) => {
+                        return this.dateConstrain(time, "BEGIN")
+                    }
+                },
+                endPickerOptions: {
+                    disabledDate: (time) => {
+                        return this.dateConstrain(time, "END")
+                    }
+                },
+            }
+        },
+        methods: {
+            // 判断开始日期与结束日期时间跨度是否在1一个月内
+            dateConstrain: function(time, order) {
+                let date = new Date(time)
+                if (date.getTime() >= Date.now())
+                    return true
+
+                if (this.beginDate != null || this.endDate != null) {
+                    if (this.beginDate != null && this.endDate != null) {
+                        let begin = new Date(this.beginDate)
+                        let end = new Date(this.endDate)
+                        return this.amongOneMonth(begin, end)
+                    } else {
+                        if (this.beginDate != null && order == "END") {
+                            var begin = new Date(this.beginDate)
+                            var end = new Date(time)
+                            return this.amongOneMonth(begin, end)
+                        }
+                        if (this.endDate != null && order == "BEGIN") {
+                            var begin = new Date(time)
+                            var end = new Date(this.endDate)
+                            return this.amongOneMonth(begin, end)
+                        }
+                    }
+                }
+
+                return false
+            },
+
+            amongOneMonth: function (begin, end) {
+                if (begin.getTime() < end.getTime()
+                    && (begin.getMonth() == end.getMonth()
+                        || ((end.getMonth()-begin.getMonth() === 1 || (begin.state.getMonth() == 11 && end.state.getMonth() == 0))
+                            && begin.getDate() >= end.getDate())))
+                    return false
+                return true
             }
         }
     }
